3fb47630cead0c789acdd307aead2747a15edba0
Lior Barkai <lior5858@gmail.com>
2025-03-24 20:58:02 +0200

    Add velocity cancelation

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -19,11 +19,19 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 CameraForward = followCamera.transform.forward.normalized;
         Vector3 CameraRight = followCamera.transform.right.normalized;
+
+        CameraForward.y = 0;
+        CameraRight.y = 0;
+
         Rb.linearVelocity = _InputDirection.x * CameraRight + _InputDirection.y * CameraForward;
     }
 
     public void OnMove(InputAction.CallbackContext context){
         if(context.performed){
+            Vector2 inputValue = context.ReadValue<Vector2>();
+            _InputDirection = new Vector3(inputValue.x,0, inputValue.y);
+        }
+        else if(context.canceled){
             Vector2 inputValue = context.ReadValue<Vector2>();
             _InputDirection = new Vector3(inputValue.x,0, inputValue.y);
         }
