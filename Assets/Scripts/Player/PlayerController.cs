04565c8a5cadaf0e8bf71aa338ad65081b898cd6
Cornetto <lior5858@gmail.com>
2025-03-25 13:22:12 +0200

    Basic character movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,9 +4,14 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Character References")]
     [SerializeField] private Rigidbody Rb;
 
     [SerializeField] private CinemachineCamera followCamera;
+
+    [Header("Character Data")]
+
+    [SerializeField] private float movementSpeed = 5;
     private Vector3 _InputDirection;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,13 +22,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 CameraForward = followCamera.transform.forward.normalized;
-        Vector3 CameraRight = followCamera.transform.right.normalized;
+        Vector3 CameraForward = followCamera.transform.forward;
+        Vector3 CameraRight = followCamera.transform.right;
 
+        // Remove vertical influence
         CameraForward.y = 0;
         CameraRight.y = 0;
 
-        Rb.linearVelocity = _InputDirection.x * CameraRight + _InputDirection.y * CameraForward;
+        // Normalize to prevent slow diagonal movement
+        CameraForward.Normalize();
+        CameraRight.Normalize();
+
+        // Corrected movement direction (z instead of y)
+        Rb.linearVelocity = (_InputDirection.x * CameraRight + _InputDirection.z * CameraForward) * movementSpeed;
     }
 
     public void OnMove(InputAction.CallbackContext context){
