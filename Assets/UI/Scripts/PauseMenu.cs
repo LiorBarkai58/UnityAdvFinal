c9283d1b0e6defefa7a0158e11e721e52635729d
yanivarditti1 <yanivarditti1@gmail.com>
2025-03-27 11:52:37 +0200

    pause menu
    - added pause unpause logic and pause menu
    - added main menu scene

--- Assets/UI/Scripts/PauseMenu.cs
+++ Assets/UI/Scripts/PauseMenu.cs
@@ -1,36 +1,39 @@
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
 
 public class PauseMenu : MonoBehaviour
 {
     public static bool IsGamePaused = false;
     [SerializeField] private GameObject pauseMenuUI;
+    [SerializeField] private PlayerController playerController;
 
     private void Start()
     {
-        Debug.Log($"pausemenu attatched to {gameObject.name}");
+        playerController.OnPlayerPause += PauseUnPause;
     }
-    void Update()
+    public void PauseUnPause()
     {
-        
-    }
-    public void Resume()
-    {
-        pauseMenuUI.SetActive(false);
-        Time.timeScale = 1f;
-        IsGamePaused = false;
+        if (IsGamePaused)
+        {
+            pauseMenuUI.SetActive(false);
+            Time.timeScale = 1f;
+            IsGamePaused = false;
 
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-        Debug.Log("resuming game");
-    }
-    void Pause()
-    {
-        pauseMenuUI.SetActive(true);
-        Time.timeScale = 0f;
-        IsGamePaused = true;
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            Debug.Log("resuming game");
+        }
+        else
+        {
+            pauseMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            IsGamePaused = true;
 
-        Cursor.visible = true;
-        Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+       
     }
 }
