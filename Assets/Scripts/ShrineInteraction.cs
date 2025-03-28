69108113caf7b7afa563eefe223b978402fad5b1
yanivarditti1 <yanivarditti1@gmail.com>
2025-03-28 17:31:42 +0300

    interactibles weeeee
    - added shrine interactible
    - logic still not complete

---
+++ Assets/Scripts/ShrineInteraction.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class ShrineInteraction : MonoBehaviour
+{
+    [SerializeField] private GameObject frogModel;
+    [SerializeField] private GameObject interationPrompt;
+    private PlayerController playerController;
+    private bool isInteractRadius;
+
+    private void HandleInteraction()
+    {
+        if (isInteractRadius)
+        {
+            Debug.Log("interaction event triggered");
+            //level up event
+            interationPrompt.SetActive(false);
+            Destroy(frogModel);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            interationPrompt.SetActive(true);
+            playerController = other.GetComponentInParent<PlayerController>();
+
+            if (playerController)
+            {
+                Debug.Log($"player controller set to {playerController}");
+            }
+
+            isInteractRadius = true;
+            Debug.Log($"player entered interaction radius {isInteractRadius}");
+            playerController.OnPlayerInteract += HandleInteraction;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("player left interaction radius");
+            interationPrompt.SetActive(false);
+            isInteractRadius = false;
+            playerController.OnPlayerInteract -= HandleInteraction;
+        }
+    }
+}
