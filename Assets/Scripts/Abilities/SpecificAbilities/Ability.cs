baa551129ee73eacbf4ebd7a91920518fc44a1bd
Cornetto <lior5858@gmail.com>
2025-03-28 22:06:12 +0300

    Add sword ability

--- Assets/Scripts/Abilities/SpecificAbilities/Ability.cs
+++ Assets/Scripts/Abilities/SpecificAbilities/Ability.cs
@@ -14,12 +14,14 @@ public abstract class Ability : MonoBehaviour {
 
     protected float _damageModifier = 1;
 
+    protected float _personalAttackSpeedMultiplier = 1;
+
 
     //Attackspeed multiplier is a way to reduce the cooldown and increase the recharge rate of abilities
     public void AbilityUpdate(float AttackSpeedMultiplier = 1)//Ran by the ability manager and no personal update function
     {
         if(onCooldown){
-            elapsedCooldown += Time.deltaTime * AttackSpeedMultiplier;
+            elapsedCooldown += Time.deltaTime * AttackSpeedMultiplier * _personalAttackSpeedMultiplier;
             if(elapsedCooldown > Cooldown){
                 onCooldown = false;
                 elapsedCooldown = 0;
--- Assets/Scripts/Abilities/SpecificAbilities/Sword.cs
+++ Assets/Scripts/Abilities/SpecificAbilities/Sword.cs
@@ -1,12 +1,30 @@
+using DG.Tweening;
 using UnityEngine;
 
 
-public class Sword : Ability
+public class Sword : RuneCircle
 {
     [SerializeField] private ParticleSystem effect;
     public override bool AbilityLogic()
     {
         effect.Play();
+        base.AbilityLogic();
         return true;
+    }
+
+    public override void UpgradeAbility()
+    {
+        base.UpgradeAbility();
+        if(_level%3 == 0){
+            _personalAttackSpeedMultiplier += 0.15f;
+        }
+        else if(_level %2 == 0){
+            Vector3 endValue = this.transform.localScale * 1.15f;
+            endValue.y = 1;
+            transform.DOScale(endValue, 0.3f);//Increase scale by 15% per second level
+        }
+        else{
+            _damageModifier+= 0.1f;//Increase damage by 10% every second level
+        }
     }
 }
