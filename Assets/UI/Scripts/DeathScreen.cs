6b6901960cd1c7fe808eb61910b6811197bb8541
yanivarditti1 <yanivarditti1@gmail.com>
2025-03-29 16:10:31 +0300

    Death screen
    - added death screen + logic

--- Assets/UI/Scripts/DeathScreen.cs
+++ Assets/UI/Scripts/DeathScreen.cs
@@ -1,25 +1,55 @@
 using UnityEngine;
 using System.Collections.Generic;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class DeathScreen : MonoBehaviour
 {
     [SerializeField] private PlayerCombatManager player;
     [SerializeField] private TextMeshProUGUI subheaderTMP;
+    [SerializeField] private GameObject hudCanvas;
     private List<string> subheaderMessages;
     
 
-    private void OnValidate()
+    private void Awake()
     {
         player.OnDeath += HandlePlayerDied;
+
+        subheaderMessages = new()
+        {
+            new("don't worry, it's ok, don't feel bad, this is a safe space and we don't judge your skill issue"),
+            new("oof, that looks like it hurt"),
+            new("git gud scrub\n(respectfully)"),
+            new("me when the")
+        };
     }
 
     private void HandlePlayerDied(CombatManager combatManager)
     {
-
+        hudCanvas.SetActive(false);
+        Time.timeScale = 0f;
+        SetSubheaderText();
     }
     private void SetSubheaderText()
     {
+        string currentDeathMessage = subheaderMessages[Random.Range(0, subheaderMessages.Count)];
+        subheaderTMP.SetText(currentDeathMessage);
+    }
 
+    public void RetryButton()
+    {
+        hudCanvas.SetActive(true);
+        Time.timeScale = 1f;
+        LevelLoader.Instance.LoadLevel(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void MainMenuButton()
+    {
+        hudCanvas.SetActive(true);
+        Time.timeScale = 1f;
+        LevelLoader.Instance.LoadLevel(0);
+    }
+    public void QuitButton()
+    {
+        Application.Quit();
     }
 }
